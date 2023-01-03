a7c92eed32d9c8921ceafda1d00a76859700b9a3
Guilherme <guilhermefonseca8426@gmail.com>
2023-01-02 21:02:17 -0300

    Q4

---
+++ Roteiro_0/PAR.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string A, B, answ;
+    int n, m1, m2, na, nb, counter=1;
+
+    while(1){
+        cin >> n;
+        if(n==0) break;
+        if(counter != 1) cout << '\n';
+
+        cin >> A >> B;
+
+        cout << "Teste " << counter << '\n';
+
+        na = 0;
+        nb = 0;
+        for(int c=0; c<n; c++){
+            cin >> m1 >> m2;
+            if((m1+m2)%2 == 0){ na=1;nb=0;}
+            else{na=0;nb=1;};
+
+            answ = na > nb ? A : B;
+            cout << answ << "\n";
+        }
+
+        counter++;
+
+    }
+
+    return 0; 
+}
