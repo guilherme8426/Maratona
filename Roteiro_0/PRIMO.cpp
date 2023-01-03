f2aa8935a19482e44826c862d18c56f8df4f7689
Guilherne Fonseca <107326368+guilherme8426@users.noreply.github.com>
2023-01-02 21:11:57 -0300

    Q5

---
+++ Roteiro_0/PRIMO.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, s = 1;
+
+    cin >> n;
+
+    for(int c=2; c<n; c++){
+        if(n%c==0)s=0;
+    }
+
+    if((s == 1)&&(n > 1)) cout << "sim" << '\n'; 
+    else cout << "nao" << '\n';
+
+    return 0; 
+}
