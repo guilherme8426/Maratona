cd1ba10e3ea5033aa39456df73519c432e337dfe
Guilherme <guilhermefonseca8426@gmail.com>
2023-01-06 18:38:11 -0300

    Q5

---
+++ Roteiro_1/URI_1151.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, f[49];
+
+    cin >> n;
+
+    if(n>=1){
+        cout << 0 << ' ';
+        f[0] = 0;
+    }
+
+    if(n>=2){
+        cout << 1 << ' ';
+        f[1] = 1;
+    }
+
+    for(int c=2; c<n; c++){
+        f[c] = f[c-1] + f[c-2];
+        cout << f[c];
+        if(c!=n-1) cout << " ";
+        else cout << '\n';
+    }
+
+    return 0;
+}
