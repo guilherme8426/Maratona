49e06e33d83e1d90f603e2664a336d50a826b042
Guilherme <guilhermefonseca8426@gmail.com>
2023-01-08 15:18:07 -0300

    Q3

---
+++ Roteiro_2/PKU_2388.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    vector<int> a;
+
+    cin >> n;
+
+    for(int c=0; c<n; c++){
+        cin >> m;
+        a.push_back(m);
+    }
+
+    sort(a.begin(), a.end());
+    if(a.size()%2==0) cout << (a[a.size()/2]+a[(a.size()/2)+1])/2 << '\n';
+    else cout << a[a.size()/2] << '\n';
+
+    return 0;
+}
